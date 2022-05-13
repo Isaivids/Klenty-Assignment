c14db148f9846115498a14ea115a7f55e4bcc6d4
IsaiVids <isaivids2020@gmail.com>
2022-05-13 14:51:15 +0530

    commit

--- pages/components/Cards.js
+++ pages/components/Cards.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 function Cards() {
 
-  const content = [
+const content = [
     {
       img: "/Group 1006-3.png" ,h1: "Never update your contacts manually", p: "Any contact you add in CRM gets updated automatically in Klenty. Set up Triggers to drive leads from Klenty back into the CRM.  Save hours everyday to focus on critical activities"
     },
@@ -29,20 +29,18 @@ function Cards() {
       {content.map((x)=>{
             return(
               <div key={x.img}>
-              <div className={styles.card1}>
-              <div className={styles.logo1} >
-              <Image src={x.img} alt="Group 1006" width="60" height="60"></Image>
-              </div>
-              <div className={styles.cardh1}>{x.h1}</div>
-              <div className={styles.cardpt1}>{x.p}</div>
+                <div className={styles.card1}>
+                  <div className={styles.logo1} >
+                    <Image src={x.img} alt="Group 1006" width="60" height="60"></Image>
+                  </div>
+                  <div className={styles.cardh1}>{x.h1}</div>
+                  <div className={styles.cardpt1}>{x.p}</div>
               </div>
             </div>
             )
           })}
-      </div>
-      
+      </div>     
     </div>
-
   )
 }
 
--- pages/components/Profiles.js
+++ pages/components/Profiles.js
@@ -7,13 +7,13 @@ const [index, setIndex] = useState(0);
 
 const profiles = [
   {
-    id: 1 , img:"/Adam.png", imgname: "Adam Weinger", desc:"President,Double The Donation ", content:"Klenty has helped us engage a far greater number of leads per week, resulting in an outbound revenue jump of 93%", logo:"/double-logo.png"
+    id: 1 , img:"/adamm.png", imgname: "Adam Weinger", desc:"President,Double The Donation ", content:"Klenty has helped us engage a far greater number of leads per week, resulting in an outbound revenue jump of 93%", logo:"/double-logo.png"
   },
   {
     id: 2 , img:"/gavin.png", imgname: "Sam W", desc:"Head of Demand Generation , Jump", content:"From someone who is relatively new to sales and lead generating, Klenty was a highly effective and simple-to-use tool that allowed me to quickly generate leads in a genuine and customisable manner", logo:"/jump-logo.png"
   },
   {
-    id: 3 , img:"/Adam.png", imgname: "Gavin Tye", desc:"Sales Director , Red Eye ", content:"It changed our business. Klenty allowed our outbounds sales team to leverage their time and skills ", logo:"/red-eye.png"
+    id: 3 , img:"/adamm.png", imgname: "Gavin Tye", desc:"Sales Director , Red Eye ", content:"It changed our business. Klenty allowed our outbounds sales team to leverage their time and skills ", logo:"/red-eye.png"
   },
 ]
 
